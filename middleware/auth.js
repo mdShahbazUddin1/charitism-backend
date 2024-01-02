bbe076230352fee5d44476a56cc4b32ffb4dbe66
jack <jackayron5@gmail.com>
2024-01-02 17:20:27 +0530

    done

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,8 +4,11 @@ const { UserModel } = require("../model/user");
 const auth = async (req, res, next) => {
   try {
     const token = req.headers?.authorization;
+    //checking token
     if (!token) return res.status(404).json({ msg: "token is not provided" });
+    // comapring token
     const decodeToken = jwt.verify(token, process.env.accessToken);
+    // finding user
     const user = await UserModel.findOne({ _id: decodeToken.userId });
     if (!user) {
       return res.status(401).json({ message: "Invalid token" });
